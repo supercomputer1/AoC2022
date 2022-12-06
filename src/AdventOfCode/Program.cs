675d645543e166a743095e7f8aa1708e774cfe08
Frans Freij <TIBOFRF@mio.local>
2022-12-06 11:25:29 +0100

    Remove comment

--- src/AdventOfCode/Program.cs
+++ src/AdventOfCode/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using AdventOfCode.Day1;
+﻿using AdventOfCode.Day1;
 using AdventOfCode.Day2;
 using AdventOfCode.Day3;
 using AdventOfCode.Day4;
